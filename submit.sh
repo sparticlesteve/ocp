c31974a48075f1408f4ae3e72438bdf66646c36b
Steve Farrell <sparticlesteve@gmail.com>
2021-06-06 09:15:53 -0700

    adding my submit script

---
+++ submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+conda activate ocp-models
+
+python main.py --config-yml configs/mlperf_hpc.yml \
+    --mode train --distributed --submit \
+    --amp \
+    --num-gpus 8 \
+    --num-workers 8 \
+    --num-nodes 2 \
+    --slurm-timeout 8
