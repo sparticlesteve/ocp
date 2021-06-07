27a17db9534ddb5ec00bc771c9837a7a4838ba3d
Steve Farrell <sparticlesteve@gmail.com>
2021-06-07 13:18:08 -0700

    trying submission with checkpoint arg

--- submit.sh
+++ submit.sh
@@ -3,10 +3,12 @@
 conda activate ocp-models
 export NCCL_SOCKET_IFNAME=eth
 
+set -x
 python main.py --config-yml configs/mlperf_hpc.yml \
     --mode train --distributed --submit \
     --amp \
     --num-gpus 8 \
     --num-workers 8 \
     --num-nodes 2 \
-    --slurm-timeout 8
+    --slurm-timeout 8 \
+    --checkpoint checkpoints/2021-06-06-18-06-08/checkpoint.pt
