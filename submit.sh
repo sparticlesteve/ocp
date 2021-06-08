6c6f87efd5a71fd2b52777fd552fe8744ba5bd2c
Steve Farrell <sparticlesteve@gmail.com>
2021-06-08 00:54:00 -0700

    spreading across more nodes to try and make it faster

--- submit.sh
+++ submit.sh
@@ -9,6 +9,7 @@ python main.py --config-yml configs/mlperf_hpc.yml \
     --amp \
     --num-gpus 8 \
     --num-workers 8 \
-    --num-nodes 2 \
-    --slurm-timeout 8 \
-    --checkpoint checkpoints/2021-06-06-18-06-08/checkpoint.pt
+    --num-nodes 4 \
+    --slurm-timeout 8
+
+#--checkpoint checkpoints/2021-06-06-18-06-08/checkpoint.pt
