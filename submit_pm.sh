81ecc119acc719d560edc0abf16dd0c32a44470e
Steve Farrell <sparticlesteve@gmail.com>
2021-09-05 20:18:22 -0700

    pm-016-n256-b4
    256 gpus, local-bs 4, global-bs 1024, accelerated LR schedule.

--- submit_pm.sh
+++ submit_pm.sh
@@ -11,7 +11,7 @@ export NCCL_IB_DISABLE=1
 export SBATCH_REQUEUE=1
 
 # Run ID
-id=pm-015-n128-b4
+id=pm-016-n256-b4
 
 set -x
 python main.py --config-yml configs/mlperf_hpc_pm.yml \
@@ -19,5 +19,5 @@ python main.py --config-yml configs/mlperf_hpc_pm.yml \
     --identifier $id \
     --num-gpus 4 \
     --num-workers 31 \
-    --num-nodes 32 \
+    --num-nodes 64 \
     --slurm-timeout 8
