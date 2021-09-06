092db36897e8891be7cebac85157d8842f450e66
Steve Farrell <sparticlesteve@gmail.com>
2021-09-06 09:35:41 -0700

    pm-019-n256-b8
    further reducing LR to 0.002 for bs 2048.

--- submit_pm.sh
+++ submit_pm.sh
@@ -11,7 +11,7 @@ export NCCL_IB_DISABLE=1
 export SBATCH_REQUEUE=1
 
 # Run ID
-id=pm-018-n256-b8
+id=pm-019-n256-b8
 
 set -x
 python main.py --config-yml configs/mlperf_hpc_pm.yml \
@@ -20,4 +20,4 @@ python main.py --config-yml configs/mlperf_hpc_pm.yml \
     --num-gpus 4 \
     --num-workers 31 \
     --num-nodes 64 \
-    --slurm-timeout 8
+    --slurm-timeout 4
