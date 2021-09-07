010463856bd093694c962fd9796b9fb7570373d3
Steve Farrell <sparticlesteve@gmail.com>
2021-09-07 12:26:05 -0700

    update submit_pm script

--- submit_pm.sh
+++ submit_pm.sh
@@ -2,17 +2,16 @@
 
 module purge
 conda activate ocp-dev
-module use /global/common/software/m1759/catalysis_dl_envs/cuda_modulefiles
+#module use /global/common/software/m1759/catalysis_dl_envs/cuda_modulefiles
 module load cuda/11.1.1
 
 # NCCL hangs otherwise..?
 export NCCL_IB_DISABLE=1
+# Enable submitit to requeue the job
+export SBATCH_REQUEUE=1
 
 # Run ID
 id=pm-010-n32-b8
-
-# Enable submitit to requeue the job
-export SBATCH_REQUEUE=1
 
 set -x
 python main.py --config-yml configs/mlperf_hpc_pm.yml \
