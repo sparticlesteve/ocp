b4966171c83d2c27f38da26a289a6165923c0a02
Steve Farrell <sparticlesteve@gmail.com>
2021-09-09 09:59:47 -0700

    update rcp-1024 to the pm-025 config
    update campaign to ocp-rcp-1024-02.

--- submit_rcp_1024.sh
+++ submit_rcp_1024.sh
@@ -12,31 +12,31 @@ export SBATCH_REQUEUE=1
 set -x
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-01 --seed 1
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-01 --seed 1
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02 --seed 2
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-02 --seed 2
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-03 --seed 3
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-03 --seed 3
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-04 --seed 4
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-04 --seed 4
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-05 --seed 5
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-05 --seed 5
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-06 --seed 6
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-06 --seed 6
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-07 --seed 7
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-07 --seed 7
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-08 --seed 8
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-08 --seed 8
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-09 --seed 9
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-09 --seed 9
 python main.py --config-yml configs/mlperf_rcp_1024.yml \
     --mode train --distributed --submit --num-gpus 4  --num-workers 31 \
-    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-10 --seed 10
+    --num-nodes 64 --slurm-timeout 4 --identifier ocp-rcp-1024-02-10 --seed 10
