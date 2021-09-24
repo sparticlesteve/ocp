ec890fa290b2dfd9b23f5cc8dbe8df4f7abe27e1
Steve Farrell <sparticlesteve@gmail.com>
2021-09-24 11:37:25 -0700

    add nv binding and profiling to run_training.sh

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -11,7 +11,25 @@ export WORLD_SIZE=$SLURM_NTASKS
 export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
 
-python main.py --mode train \
+# NVIDIA's binding script
+if [ "${ENABLE_NV_BINDING:-0}" -eq 1 ]; then
+    if [ "${NERSC_HOST}" -eq "perlmutter" ]; then
+        bind_map=scripts/pm_bind_map.sh
+        BIND_CMD="./scripts/bind.sh --cpu=$bind_map --mem=$bind_map --"
+    else
+        BIND_CMD="./scripts/bind.sh --cpu=exclusive --"
+    fi
+fi
+
+# Profiling
+if [ "${ENABLE_PROFILING:-0}" -eq 1 ]; then
+    PROFILE_DIR=${PROFILE_DIR:-$SCRATCH/ocp/profile}
+    NSYS_ARGS=${NSYS_ARGS:---trace=cuda,cublas,nvtx --kill none -c cudaProfilerApi -f true}
+    PROFILE_CMD="nsys profile $NSYS_ARGS -o $PROFILE_DIR/${id}"
+    mkdir -p $PROFILE_DIR
+fi
+
+${BIND_CMD} ${PROFILE_CMD} python main.py --mode train \
     --distributed \
     --local_rank $LOCAL_RANK \
     --identifier $id $args
