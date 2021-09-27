a9cc6eb7a7a5a7e64b7b20960c96c86fa3bafc78
Steve Farrell <sparticlesteve@gmail.com>
2021-09-27 16:48:29 -0700

    perf test submit script

---
+++ scripts/submit_perf_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Performance test on 1gpu and 4gpus
+export OMP_NUM_THREADS=1
+export WANDB_MODE="dryrun"
+export OCP_CONFIG=configs/perf_test_prof.yml
+export ENABLE_PROFILING=1
+export ENABLE_NV_BINDING=1
+sbatch -J pm-prof -t 30 --ntasks-per-node=1 -d singleton --exclusive \
+    scripts/train_pm_shifter.sh
+# Single node profiling
+sbatch -J pm-prof -t 30 --ntasks-per-node=4 -d singleton --exclusive \
+    scripts/train_pm_shifter.sh
+
+# Single GPU tests
+#export OCP_CONFIG=configs/perf_test_n1.yml
+#args="-J pm-perf -d singleton -t 30 -n 1"
+#sbatch $args scripts/train_pm_shifter.sh
+#sbatch $args scripts/train_pm_shifter.sh --name="'dpp_opts'"
+#sbatch $args scripts/train_pm_shifter.sh --name="'dpp_opts'" --optimizer="'FusedAdam'"
+#sbatch $args scripts/train_pm_shifter.sh --name="'dpp_opts'" --optimizer="'FusedAdam'" --amp
+
+## Single-gpu to 4-gpu comparison on cori-gpu
+#export WANDB_MODE="dryrun"
+#export OCP_CONFIG=configs/perf_test_5k.yml
+#export ENABLE_PROFILING=0
+#export ENABLE_NV_BINDING=1
+#sbatch -J cgpu-prof -t 30 --ntasks-per-node=1 -d singleton --exclusive \
+#    scripts/train_cgpu_shifter.sh --max_epochs=1
+#sbatch -J cgpu-prof -t 30 --ntasks-per-node=8 -d singleton --exclusive \
+#    scripts/train_cgpu_shifter.sh --max_epochs=2
+
