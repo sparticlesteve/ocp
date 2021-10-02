5e8a38305cc997073c592fbde932b610005fcf2e
Steve Farrell <sparticlesteve@gmail.com>
2021-10-02 12:45:23 -0700

    update perf tests submit script

--- scripts/submit_perf_tests.sh
+++ scripts/submit_perf_tests.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-# Performance test on 1gpu and 4gpus, with/without nvidia (fixed) binding
-export OMP_NUM_THREADS=1
+#-------------------------------------------------------------------------------
+# Scanning for best omp_num_threads and num_workers
 export OCP_CONFIG=configs/perf_test_5k.yml
-#export ENABLE_PROFILING=1
-export ENABLE_NV_BINDING=0
-sbatch -J pm-prof -t 30 --ntasks-per-node=1 -d singleton --exclusive scripts/train_pm_shifter.sh
-sbatch -J pm-prof -t 30 --ntasks-per-node=4 -d singleton --exclusive scripts/train_pm_shifter.sh
-export ENABLE_NV_BINDING=1
-sbatch -J pm-prof -t 30 --ntasks-per-node=1 -d singleton --exclusive scripts/train_pm_shifter.sh
-sbatch -J pm-prof -t 30 --ntasks-per-node=4 -d singleton --exclusive scripts/train_pm_shifter.sh
+export ENABLE_PROFILING=1
+#for ompnt in 1 2 4 8 16; do
+for ompnt in 1 32 64; do
+    export OMP_NUM_THREADS=$ompnt
+    #for nio in 4 8 16; do
+    for nio in 1 2; do
+        sbatch -J pm-perf-omp${ompnt}-io${nio} -t 10 -d singleton --exclusive \
+            --ntasks-per-node=1 \
+            scripts/train_pm_shifter.sh --num_workers=$nio
+    done
+done
+
+#-------------------------------------------------------------------------------
+# Performance test on 1gpu and 4gpus, with/without nvidia (fixed) binding
+#export OMP_NUM_THREADS=1
+#export OCP_CONFIG=configs/perf_test_5k.yml
+##export ENABLE_PROFILING=1
+#export ENABLE_NV_BINDING=0
+#sbatch -J pm-prof -t 30 --ntasks-per-node=1 -d singleton --exclusive scripts/train_pm_shifter.sh
+#sbatch -J pm-prof -t 30 --ntasks-per-node=4 -d singleton --exclusive scripts/train_pm_shifter.sh
+#export ENABLE_NV_BINDING=1
+#sbatch -J pm-prof -t 30 --ntasks-per-node=1 -d singleton --exclusive scripts/train_pm_shifter.sh
+#sbatch -J pm-prof -t 30 --ntasks-per-node=4 -d singleton --exclusive scripts/train_pm_shifter.sh
 
 #-------------------------------------------------------------------------------
 
